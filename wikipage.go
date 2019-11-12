aed7e8f785ce3cb491955f80c8a8e1fae9bf60e3
Enrico Bonetti Vieno <entanglement.ebv@gmail.com>
2019-11-12 17:42:06 +0100

    Revert back to single goroutine

--- wikipage.go
+++ wikipage.go
@@ -120,12 +120,7 @@ func (rh RequestHandler) wakeUp() {
 		}
 		expLen = (7*expLen + 9*len(requests) + 8) / 16
 
-		//If running at full capacity, use a goroutine to handle the request
-		if len(requests) == cap(requests) {
-			go rh.handle(requests)
-		} else {
-			rh.handle(requests)
-		}
+		rh.handle(requests)
 	}
 }
 
